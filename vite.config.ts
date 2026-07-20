8c1a90bfe39327b9d56d868899b3e31136a3e4e4
Tomasz Wyrowiński <tomwyr@hotmail.com>
2026-07-21 00:08:40 +0200

    Add deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import swiftWasm from "@elementary-swift/vite-plugin-swift-wasm";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES_BASE_PATH
+    ? `${process.env.GITHUB_PAGES_BASE_PATH}/`
+    : "/",
   plugins: [
     swiftWasm({
       useEmbeddedSDK: true,
